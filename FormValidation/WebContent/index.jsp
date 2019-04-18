c184b9b4ec364a0e87ca4d2629e0ddf91d19a741
Souradip <souradip.ece.imps@gmail.com>
2019-04-19 00:55:55 +0530

    updated code

--- FormValidation/WebContent/index.jsp
+++ FormValidation/WebContent/index.jsp
@@ -8,44 +8,66 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="new.js"></script>
 <title>Flight booking System</title>
-<h1>Welcome to Flight booking System</h1>
+</marquee><h1>Welcome to Flight booking System</h1></marquee>
 </head>
 <body BACKGROUND="image.jpeg">
 <form > 
 <label>SourcePoint</label>   
-<select name="SourcePoint" >  
+<select id="splace" name="SourcePoint" onchange="verifyPlaces()">  
 <option>India</option>  
 <option>Pakistan</option>  
 <option>China</option> 
 <option>Other</option> 
 </select>
 <label>DestinationPoint</label> 
-<select name="DestinationPoint" >  
+<select id="dplace" name="DestinationPoint" onchange="verifyPlaces()">  
 <option>India</option>  
 <option>Pakistan</option>  
 <option>China</option> 
 <option>Other</option>  
 </select> 
+<span id="errplace"></span>
 <label>Start Date: </label>
-<input type="date" id="stdate" onchange="removeSdaterr()">
+<input type="date" id="stdate" onchange="selectdate()">
 <span id="errstdate"></span>
 </p>
 <p>
 <label>Return Date: </label>
-<input type="date" id="rtdate" name="rday" onchange="removeRdaterr()">
-<span id="errrtdate"></span>
+<input type="date" id="rtdate" name="rday" onchange="selectdate()">
+<span id="errrtdate"></span> 
 </p>
 enter email:<input type="text" name="userEmail"/>
 enter number of seats to book:<input type="number" name="NumberOfTicket" id="numberofticket" value=""/> 
 <button type="button" class="Seats availability" name="Seats availability" onclick="ticketsAvailability()">Check seats availability</button>
-
+<div id="passform">
+</div>
 <p id="message"></p>
-
+<input id="prcbtn" type="submit" onclick="getPrice()" value="Estimate Price" disabled>
+<input id="subbtn" type="submit" onclick="succMsg()" value="Book Now" disabled>
 
 <div id="container">
 </div>
+<p>
+<label>Number of Passengers: </label>
+  <input type="text" name="passengers" maxlength="2" size="1" id="passdet" value="" onkeypress="return isNumber(event)">
+  <input id="addpass" type="submit" class="btn btn-large btn-primary" onclick="addTables()" value="Add" disabled>
+  <span id="tablerr"></span>
+</p>
+<!--  <marquee behavior="alternate"><img src="AirIndia.jpg" href="http://www.airindia.in/"
+src="SpiceJetAirliness.jpg" href="https://www.spicejet.com/"-->
+c></marquee>
+<!--<marquee behavior="alternate"><img src="SpiceJetAirliness.jpg" href="https://www.spicejet.com/"></marquee>
+<marquee behavior="alternate"><img src="Indigo.jpg" href="https://www.goindigo.in/" ></marquee>-->
+<marquee >
+<div style="width:900;  height:250px; overflow:scroll; overflow-x: scroll;overflow-y: hidden;">
 
+      <img src="AirIndia.jpg" nlick()="AirIndia()"/>
+      <img src="SpiceJetAirliness.jpg" ="https://www.spicejet.com/"/>
+      <img src="Indigo.jpg" href="https://www.goindigo.in/" >
+     
 
+    </div>
+    </marquee>
 <!-- enter name:<input type="text" name="Name"/> 
 <label>Gender</label> 
 <select name="Gender" >  
--- FormValidation/WebContent/new.js
+++ FormValidation/WebContent/new.js
@@ -17,7 +17,7 @@ function ticketsAvailability1() {
 }
 function ticketsAvailability() {
 	var ticketsCount = document.getElementById('numberofticket').value;
-	var availableTickets = 50 - parseInt(ticketsCount);
+	var availableTickets = 51 - parseInt(ticketsCount);
     var container = document.getElementById("container");
 	if(availableTickets > 0 ){
 	  	//alert("Congratulation " + availableTickets+  " tickets is available");
@@ -73,3 +73,129 @@ function removeSdaterr(){
 function removeRdaterr(){
 	document.getElementById('errrtdate').innerHTML=""; 
 }
+
+function selectdate(){
+//	var todaysDate = new Date(); // Gets today's date
+//    // Max date attribute is in "YYYY-MM-DD".  Need to format today's date accordingly
+//    var year = todaysDate.getFullYear(); 
+//    var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2);  // MM
+//    var day = ("0" + todaysDate.getDate()).slice(-2);           // DD
+//    var minDate = (year +"-"+ month +"-"+ day); // Results in "YYYY-MM-DD" for today's date 
+//    // Now to set the max date value for the calendar to be today's date
+//    $('.calendarDate input').attr('min',minDate);
+//   $(document).ready(function(){
+//	   $("#stdate").datepcker({
+//		   minDate:-1
+//	   });
+//   });
+   $(function () {
+       var date = new Date();
+       var currentMonth = date.getMonth(); // current month
+       var currentDate = date.getDate(); // current date
+       var currentYear = date.getFullYear(); //this year
+       $("#stdate").datepicker({
+           changeMonth: true, // this will allow users to chnage the month
+           changeYear: true, // this will allow users to chnage the year
+           minDate: new Date(currentYear, currentMonth, currentDate)
+          
+       });
+   });
+}
+function verifyPlaces(){
+	var p1 = document.getElementById("splace").value;
+	var p2 = document.getElementById("dplace").value;
+	var pl=0;
+	if(p1==p2){
+		document.getElementById('errplace').innerHTML="Source and Destination cannot be of the same place!";
+		pl=1;
+	}else{
+		document.getElementById('errplace').innerHTML="";
+	}
+	document.getElementById('addpass').disabled=true;
+	clearFields();
+	removeEntryTables();
+	return pl;
+}
+function clearFields() {
+	var container, inputs, index;
+
+	container = document.getElementById('passform');
+	inputs = container.getElementsByTagName('input');
+	while (inputs[0]) {
+		inputs[0].remove();
+	}
+	textin = container.getElementsByTagName('label');
+	while (textin[0]) {
+		textin[0].remove();
+	}
+	document.getElementById('passdet').value="";
+	document.getElementById('passdet').disabled=true;
+}
+
+function removeEntryTables(){
+	var container = document.getElementById("passform");
+	// Clear previous contents of the container
+	while (container.hasChildNodes()) {
+		container.removeChild(container.lastChild);
+	}	
+}
+function getPrice(){
+	var tbldiv = document.getElementById('passform');
+	var tblrow = tbldiv.getElementsByTagName('tr');
+	var price;
+	for (var i = 1; i < tblrow.length; i++) {
+                price=2000;
+                var d2 = document.getElementById("rtdate").value;
+                if(d2==""){
+                price = price/2;
+                }
+		var age = tblrow[i].getElementsByTagName('td')[2].getElementsByTagName('input')[0].value;
+		var gender = tblrow[i].getElementsByTagName('td')[1].getElementsByTagName('select')[0].value;
+		if(age>60){
+			price = price-(.10*price);
+		}else if(age<=11){
+			price = 0;
+		}else if(gender=="Female"){
+			price = price-(.05*price);
+		}
+		tblrow[i].getElementsByTagName('td')[3].getElementsByTagName('input')[0].value=price;
+	}
+	var container = document.getElementById("totalspan");
+	var txt = document.createElement("label");
+	txt.innerHTML = "Total: ";
+	container.appendChild(txt)
+	// Create an <input> element, set its type and name attributes
+	var input = document.createElement("input");
+	input.type = "text";
+	input.value = totalPrice();
+	input.readOnly=true;
+	container.appendChild(input);
+	document.getElementById('subbtn').disabled=false;
+}
+
+function totalPrice(){
+	var tbldiv = document.getElementById('passform');
+	var tblrow = tbldiv.getElementsByTagName('tr');
+	var tot=parseInt("0") ;
+	for (var i = 1; i < tblrow.length; i++) {
+		tot=tot + parseInt(tblrow[i].getElementsByTagName('td')[3].getElementsByTagName('input')[0].value);
+	}
+	return tot;
+}
+function AirIndia(){
+	document.open("http://www.airindia.in/");
+}
+
+$(document).ready(function(){
+	var dtToday = new Date(); 
+	var month = dtToday.getMonth() + 1;
+	var day = dtToday.getDate();
+	var year = dtToday.getFullYear();
+	if(month < 10)
+		month = '0' + month.toString();
+	if(day < 10)
+		day = '0' + day.toString(); 
+	var maxDate = year + '-' + month + '-' + day;
+	$('#stdate').attr('min', maxDate);
+	$('#rtdate').attr('min', maxDate);
+});
